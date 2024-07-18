eadac1cc70ac278e90c578e9097967510454e686
facubotta <nutuestampados@gmail.com>
2024-07-18 10:00:01 +0200

    parallel routes comming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,15 +21,17 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <section className="flex items-center justify-center w-full min-h-screen bg-light-ciel dark:bg-dark-bg">
-        <div className="h-full w-[90%] flex flex-col items-center lg:items-start gap-4 mb-[100px] text-dark-bg dark:text-dark-grey lg:mr-5 sm:w-[70%] lg:w-[40%] max-w-[500px] ">
-          <h1 className="text-[60px] sm:text-[80px] font-bold text-nowrap dark:text-dark-greenLight">EventHub</h1>
-          <p><strong>EventHub</strong> est votre solution tout-en-un pour la gestion d&apos;événements et de RDV. Simplifiez l&apos;organisation de vos activités avec notre interface moderne et intuitive. Avec EventHub, vous pouvez facilement synchroniser vos événements avec Google Calendar, gérer vos tâches, et communiquer efficacement avec vos collègues. Que vous planifiez un petit rassemblement ou un grand événement, EventHub vous offre les outils nécessaires pour réussir !</p>
-          <div className="flex gap-8">
-            <Button onClick={ () => showForm('Sign-In') }>SignIn!</Button>
-            <Button onClick={() => showForm('Sign-Up')} >SignUp!</Button>
+        <div className="flex h-full w-[90%] m-auto items-center justify-center text-center lg:!text-start">
+          <div className="text-dark-bg dark:text-dark-grey lg:mr-10 sm:w-[70%] lg:w-[40%] max-w-[500px] ">
+            <h1 className="text-[60px] mt-[-20px] sm:text-[80px] font-bold text-nowrap dark:text-dark-greenLight">EventHub</h1>
+            <p className="text-balance"><strong>EventHub</strong> est votre solution tout-en-un pour la gestion d&apos;événements et de RDV. Simplifiez l&apos;organisation de vos activités avec notre interface moderne et intuitive. Avec EventHub, vous pouvez facilement synchroniser vos événements avec Google Calendar, gérer vos tâches, et communiquer efficacement avec vos collègues. Que vous planifiez un petit rassemblement ou un grand événement, EventHub vous offre les outils nécessaires pour réussir !</p>
+            <div className="flex gap-8 mt-4 justify-center lg:!justify-start">
+              <Button onClick={ () => showForm('Sign-In') }>SignIn!</Button>
+              <Button onClick={() => showForm('Sign-Up')} >SignUp!</Button>
+            </div>
           </div>
+          <HomeImage className="hidden lg:block w-[500px] h-full" theme={mounted ? theme : "dark"}/>
         </div>
-        <HomeImage className="hidden lg:block w-[500px]" theme={mounted ? theme : "dark"}/>
       </section>
       {
         formOpen.open && (
--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,17 +1,18 @@
-"use client"
+// "use client"
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 export default function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false);
+  // const [mounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
+  console.log(systemTheme)
   const currentTheme = theme === 'system' ? systemTheme : theme;
 
-  useEffect(() => {
+  /* useEffect(() => {
     setMounted(true);
   }, [])
 
-  if(!mounted) return null
+  if(!mounted) return null */
 
   return (
     <button
